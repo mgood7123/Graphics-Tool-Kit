e9b9d72bcd66a47d6dbe2b8c9ad4c5883b099604
mgood7123 <smallville7123@gmail.com>
2021-06-30 07:30:51 +1000

    undelete DiligentAppBase

--- app/src/main/cpp/helpers/DiligentAppBase.cpp
+++ app/src/main/cpp/helpers/DiligentAppBase.cpp
@@ -5,3 +5,22 @@
 #include "DiligentAppBase.h"
 #include <EngineFactoryOpenGL.h>
 
+DiligentAppBase::DiligentAppBase ()
+{
+    m_DeviceType = Diligent::RENDER_DEVICE_TYPE_GL;
+}
+
+DiligentAppBase::~DiligentAppBase ()
+{
+}
+
+void DiligentAppBase::attachToContext (int32_t w, int32_t h)
+{
+    Diligent::EngineGLCreateInfo EngineCI;
+    
+    Diligent::GetEngineFactoryOpenGL()
+            ->CreateSwapChainGLAndAttachToActiveGLContext(
+                    EngineCI, &m_pDevice, &m_pImmediateContext,
+                    m_pSCDesc, &m_pSwapChain, w, h
+            );
+}
