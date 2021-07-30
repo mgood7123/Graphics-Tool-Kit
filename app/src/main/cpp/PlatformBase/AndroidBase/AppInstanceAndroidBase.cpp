2aa71a8b9226d29ebd33d8d68dc28c56951548d1
smallville7123 <smallville7123@gmail.com>
2021-07-30 19:59:25 +1000

    remove MotionEvent from AndroidBase

--- app/src/main/cpp/PlatformBase/AndroidBase/AppInstanceAndroidBase.cpp
+++ app/src/main/cpp/PlatformBase/AndroidBase/AppInstanceAndroidBase.cpp
@@ -55,56 +55,6 @@ void AppInstanceAndroidBase::onEglSetup (JNIEnv * jenv, jobject classInstance, j
     }
 }
 
-bool AppInstanceAndroidBase::onTouchEvent (JNIEnv * jenv, jfloatArray motionEventData)
-{
-    motionEvent.acquire(jenv, motionEventData);
-    bool r;
-    int action = motionEvent.getAction(0);
-    switch (action) {
-        case MotionEventSerializer::MOTION_EVENT_ACTION_DOWN:
-        case MotionEventSerializer::MOTION_EVENT_ACTION_UP: {
-            /* On a touch screen move events aren't reported when the
-               finger is moving above (of course), so remember the position
-               always */
-            previous = {motionEvent.getX(0), motionEvent.getY(0)};
-            current = previous;
-            // MouseEvent e(motionEvent, x, y, motionEvent.getButtonState());
-            if (action == MotionEventSerializer::MOTION_EVENT_ACTION_DOWN) {
-                // mousePressEvent(e);
-            } else {
-                // mouseReleaseEvent(e);
-                // reset the relative position
-                // if the relative position is not reset, then
-                // the relative position of mouse press will be
-                // relative to the last location of mouse release
-                // which differs from desktop behaviour
-                previous = {-1,-1};
-            }
-            r = true;
-            break;
-        }
-        
-        case MotionEventSerializer::MOTION_EVENT_ACTION_MOVE: {
-            current = {motionEvent.getX(0), motionEvent.getY(0)};
-            // MouseMoveEvent e{motionEvent, x, y, motionEvent.getButtonState(),
-            //                  _previousMouseMovePosition == Vector2i{-1} ? Vector2i{} :
-            //                  position - _previousMouseMovePosition};
-            previous = current;
-            // mouseMoveEvent(e);
-            r = true;
-            break;
-        }
-        default:
-            r = false;
-            break;
-    }
-    
-    /** @todo Implement also other input events */
-    
-    motionEvent.release(jenv, motionEventData);
-    return r;
-}
-
 void AppInstanceAndroidBase::onEglTearDown ()
 {
     jvmManager.jenv->DeleteGlobalRef(jClass);
--- app/src/main/cpp/PlatformBase/AndroidBase/AppInstanceAndroidBase.h
+++ app/src/main/cpp/PlatformBase/AndroidBase/AppInstanceAndroidBase.h
@@ -6,31 +6,20 @@
 #define GRAPHICAL_TOOL_KIT_APPINSTANCEANDROIDBASE_H
 
 #include "JVM_MANAGER.h"
-#include "MotionEventSerializer.h"
 
 class AppInstanceAndroidBase
 {
 public:
-    struct MousePos {
-        float x;
-        float y;
-    };
-    
     JVM_MANAGER jvmManager;
     jobject jObject;
     jclass jClass;
     jmethodID jSwapBuffers;
-    MotionEventSerializer motionEvent;
-    
-    MousePos previous;
-    MousePos current;
-    
+
     // delete called on non-final that has virtual functions but non-virtual destructor
     virtual ~AppInstanceAndroidBase() = default;
     virtual void swapBuffers();
 
     virtual void onEglSetup(JNIEnv *jenv, jobject classInstance, jstring name, jstring signature);
-    virtual bool onTouchEvent(JNIEnv *jenv, jfloatArray motionEventData);
     virtual void onEglTearDown();
 };
 
