9eb7a1c9970636553a89e06c9d522dc30707bd0b
smallville7123 <smallville7123@gmail.com>
2021-08-07 21:10:31 +1000

    small fix to comment

--- app/src/main/cpp/TimeEngine/TimeEngine.h
+++ app/src/main/cpp/TimeEngine/TimeEngine.h
@@ -23,16 +23,20 @@ public:
     double difference;
     double elapsed;
 
-    // 1000/60 is 16.6666666667 ms
-    // 1000/16.6666666667 is 59.9999999999 fps
-    //
-    // 1/60 is 0.01666666666 ms in seconds
-    // 1/0.01666666666 is 60.000000024 fps
-    //
-    //
-    // a physics thread will ALWAYS be slower than a draw thread
-    // since the physics call IS NOT guaranteed to be called
-    // every single frame due to thread scheduling
+    /**
+     * this value is set in seconds
+     *
+     * 1000/60 is 16.6666666667 ms
+     * 1000/16.6666666667 is 59.9999999999 fps
+     *
+     * 1/60 is 0.01666666666 ms in seconds
+     * 1/0.01666666666 is 60.000000024 fps
+     *
+     *
+     * a physics thread will ALWAYS be slower than a draw thread
+     * since the physics call IS NOT guaranteed to be called
+     * every single frame due to thread scheduling
+     */
     double physicsTimeStep;
 
     bool logPhysics;
